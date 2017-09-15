485641fed9842555f95d587f07dd0e622e3d6900
D <dt@Ds-MacBook-Pro.local>
2017-09-15 19:45:06 -0400

    Initial commit

---
+++ homepage/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    # return HttpResponse
+    return render(request, 'index.html')
+
+def about_us(request):
+
+    return render(request, 'about_us.html')
